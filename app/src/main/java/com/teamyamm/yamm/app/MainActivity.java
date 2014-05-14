aa87cfcce20cd84cb5acaa4475817d3161afae63
jihopark <lastspurt17@gmail.com>
2014-05-14 16:58:16 +0900

    Implemented DrawerLayout

--- app/src/main/java/com/teamyamm/yamm/app/MainActivity.java
+++ app/src/main/java/com/teamyamm/yamm/app/MainActivity.java
@@ -1,9 +1,15 @@
 package com.teamyamm.yamm.app;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends BaseActivity {
+    private String[] navMenuTitles;
+    private DrawerLayout drawerLayout;
+    private ListView leftDrawer;
 
 
     @Override
@@ -11,6 +17,15 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        navMenuTitles = getResources().getStringArray(R.array.nav_menu_titles);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        leftDrawer = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        leftDrawer.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.left_drawer_item, navMenuTitles));
+        // Set the list's click listener
+       // mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
     }
 
 
