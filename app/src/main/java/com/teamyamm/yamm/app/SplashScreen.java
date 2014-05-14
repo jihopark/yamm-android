01a72e151518d32a0135d71a1db6a9554659a50f
jihopark <lastspurt17@gmail.com>
2014-05-14 15:10:18 +0900

    SharedPref Manipulation Added (To be removed later in production)

--- app/src/main/java/com/teamyamm/yamm/app/SplashScreen.java
+++ app/src/main/java/com/teamyamm/yamm/app/SplashScreen.java
@@ -52,7 +52,8 @@ public class SplashScreen extends Activity {
 
         // To Remove later
         BaseActivity.putInPref(prefs, getString(R.string.PREVIOUS_ACTIVITY),getString(R.string.PREVIOUS_ACTIVITY_INTRO));
-
+        prefs.edit().remove(getString(R.string.BATTLE_RESULTS)).commit();
+        Log.w("SplashScreen","Delete SharedPreference manipulation in production");
 
         String value = prefs.getString(getString(R.string.PREVIOUS_ACTIVITY),"none");
         Log.v("SplashScreen","Activity Pref value:"+value);
