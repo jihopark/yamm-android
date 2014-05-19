a8ba30956104e391fd91c86c4bcbccf161ae54ef
jihopark <lastspurt17@gmail.com>
2014-05-19 21:41:34 +0900

    Created Friends/Team Listviews

--- app/src/main/java/com/teamyamm/yamm/app/FriendsFragment.java
+++ app/src/main/java/com/teamyamm/yamm/app/FriendsFragment.java
@@ -5,16 +5,44 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 /**
  * Created by parkjiho on 5/19/14.
  */
 public class FriendsFragment extends Fragment {
+    ListView yammTeamList, yammFriendsList;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        RelativeLayout layout = (RelativeLayout) inflater.inflate(R.layout.friends_fragment, container, false);
+        LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.friends_fragment, container, false);
+
+        yammTeamList = (ListView) layout.findViewById(R.id.yamm_team_list);
+        yammFriendsList = (ListView) layout.findViewById(R.id.yamm_friends_list);
+        setYammTeamList();
+        setYammFriendsList();
+
+
 
         return layout;
+    }
+
+    private void setYammTeamList(){
+        TextView tv = new TextView(getActivity());
+
+        //Set Empty TextView
+        tv.setText(getString(R.string.team_list_empty));
+        yammTeamList.setEmptyView(tv);
+
+    }
+
+    private void setYammFriendsList(){
+        TextView tv = new TextView(getActivity());
+
+        //Set Empty TextView
+        tv.setText(getString(R.string.friends_list_empty));
+        yammFriendsList.setEmptyView(tv);
     }
 }
