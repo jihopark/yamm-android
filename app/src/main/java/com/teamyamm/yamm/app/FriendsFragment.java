f35a3c4da1c07c55ce7d8478fd9db0a40b007df0
jihopark <lastspurt17@gmail.com>
2014-05-26 11:43:43 +0900

    Refactored layout structure & methods in FriendFragment

--- app/src/main/java/com/teamyamm/yamm/app/FriendsFragment.java
+++ app/src/main/java/com/teamyamm/yamm/app/FriendsFragment.java
@@ -6,7 +6,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -15,61 +14,37 @@ import com.applidium.headerlistview.SectionAdapter;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by parkjiho on 5/19/14.
  */
 public class FriendsFragment extends Fragment {
 
-    public LinearLayout layout;
     RelativeLayout yammItemLayout;
 
     public HeaderListView headerView;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        layout = (LinearLayout) inflater.inflate(R.layout.friends_fragment, container, false);
+        yammItemLayout = (RelativeLayout) inflater.inflate(R.layout.friends_fragment, container, false);
 
         headerView = new HeaderListView(getActivity());
-        yammItemLayout = (RelativeLayout) layout.findViewById(R.id.yamm_item_list_layout);
 
 
         setYammItemList();
         yammItemLayout.addView(headerView);
 
-        return layout;
+        return yammItemLayout;
     }
 
 
     private void setYammItemList(){
-        ArrayList<YammItem> teamList = new ArrayList<YammItem>();
-        ArrayList<YammItem> friendList = new ArrayList<YammItem>();
         ArrayList<String> headerList = new ArrayList<String>();
+        List<YammItem> teamList = loadTeamList();
+        List<YammItem> friendList = loadFriendList();
 
-        teamList.add(new Team(1, "가족"));
-        teamList.add(new Team(2, "얌팀"));
-        teamList.add(new Team(3, "민사12기"));
-        teamList.add(new Team(4, "맛집투어"));
-        teamList.add(new Team(4, "맛집투어"));
-        teamList.add(new Team(5, "맛집투어"));
-        teamList.add(new Team(6, "맛집투어"));
-        teamList.add(new Team(7, "맛집투어"));
-        teamList.add(new Team(8, "맛집투어"));
-
-        friendList.add(new Friend(1, "양영직"));
-        friendList.add(new Friend(2, "박지호"));
-        friendList.add(new Friend(3, "김홍"));
-        friendList.add(new Friend(4, "고서우"));
-        friendList.add(new Friend(5, "방소정"));
-        friendList.add(new Friend(6, "황준식"));
-        friendList.add(new Friend(7, "임창균"));
-        friendList.add(new Friend(8, "한고은"));
-        friendList.add(new Friend(9, "한지은"));
-        friendList.add(new Friend(10, "박성호"));
-        friendList.add(new Friend(11, "박민선"));
-        friendList.add(new Friend(12, "임아람"));
-        friendList.add(new Friend(13, "김미정"));
-        Collections.sort(friendList);
+        //Adding Dummy Item if size is 0 for any list
 
         if (teamList.size() == 0){
             teamList.add(new Team(-1));
@@ -121,4 +96,40 @@ public class FriendsFragment extends Fragment {
             }
         });
     }
+
+    private List<YammItem> loadFriendList(){
+        ArrayList<YammItem> friendList = new ArrayList<YammItem>();
+        friendList.add(new Friend(1, "양영직"));
+        friendList.add(new Friend(2, "박지호"));
+        friendList.add(new Friend(3, "김홍"));
+        friendList.add(new Friend(4, "고서우"));
+        friendList.add(new Friend(5, "방소정"));
+        friendList.add(new Friend(6, "황준식"));
+        friendList.add(new Friend(7, "임창균"));
+        friendList.add(new Friend(8, "한고은"));
+        friendList.add(new Friend(9, "한지은"));
+        friendList.add(new Friend(10, "박성호"));
+        friendList.add(new Friend(11, "박민선"));
+        friendList.add(new Friend(12, "임아람"));
+        friendList.add(new Friend(13, "김미정"));
+        Collections.sort(friendList);
+
+        return friendList;
+    }
+
+    private List<YammItem> loadTeamList(){
+        ArrayList<YammItem> teamList = new ArrayList<YammItem>();
+        teamList.add(new Team(1, "가족"));
+        teamList.add(new Team(2, "얌팀"));
+        teamList.add(new Team(3, "민사12기"));
+        teamList.add(new Team(4, "맛집투어"));
+        teamList.add(new Team(4, "맛집투어"));
+        teamList.add(new Team(5, "맛집투어"));
+        teamList.add(new Team(6, "맛집투어"));
+        teamList.add(new Team(7, "맛집투어"));
+        teamList.add(new Team(8, "맛집투어"));
+
+        return teamList;
+    }
+
 }
