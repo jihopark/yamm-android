75e5d17981b043cbe76b61761e4c323a4daeab3a
jihopark <lastspurt17@gmail.com>
2014-05-26 11:35:23 +0900

    Created FriendActivity & Implemented FriendsFragment -2

---
+++ app/src/main/java/com/teamyamm/yamm/app/FriendActivity.java
@@ -0,0 +1,27 @@
+package com.teamyamm.yamm.app;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.widget.RelativeLayout;
+
+
+/**
+ * Created by parkjiho on 5/26/14.
+ */
+public class FriendActivity extends BaseActivity {
+    FriendsFragment friendsFragment;
+    RelativeLayout friendFragmentContainer;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend);
+
+        friendsFragment = new FriendsFragment();
+
+        FragmentTransaction tact = getSupportFragmentManager().beginTransaction();
+        tact.add(R.id.friends_fragment_container, friendsFragment);
+        tact.commit();
+    }
+}
