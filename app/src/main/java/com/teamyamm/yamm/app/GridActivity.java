8656c651e2361d9b9e17132e4c0b004568ebc01e
jihopark <lastspurt17@gmail.com>
2014-06-01 21:02:24 +0900

    Added confirm button to GridActivity

--- app/src/main/java/com/teamyamm/yamm/app/GridActivity.java
+++ app/src/main/java/com/teamyamm/yamm/app/GridActivity.java
@@ -4,6 +4,9 @@ import android.app.Dialog;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 /**
@@ -27,8 +30,52 @@ public class GridActivity extends BaseActivity {
         goBackHome();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.grid_activity_actions, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.grid_confirm_button:
+                Log.i("GridActivity/OnOptionsItemSelected","Confirm Grid Button Clicked");
+                finishGridActivity();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     ////Private Methods
+
+    /*
+    * Finshes GridActivity and saves&sends gridresults and go to BattleActivity
+    * */
+    private void finishGridActivity(){
+        boolean resultSent = true;
+        Log.i("GridActivity/finishGridActivity", "FinishGridActivity Started");
+
+        //Save to Shared Pref
+        String result = saveGridResult(gridFragment);
+
+        //Send to Server
+
+        if (!sendGridResult(result)){
+            Log.e("Server Communication Error", "Sending Battle Results Failed");
+            showInternetConnectionAlert(new CustomInternetListener(internetAlert));
+            resultSent=false;
+        }
+        //If sendBattle Result Failed, don't go to Battle Activity
+        if (resultSent!=false) {
+            goToActivity(BattleActivity.class);
+        }
+    }
 
     /*
     * Send Grid Selected Result to server
@@ -57,7 +104,7 @@ public class GridActivity extends BaseActivity {
             if (checkInternetConnection()) {
                 Log.i("GridActivity/CustomInternetListener","Internet came back");
                 dialog.dismiss();
-               // finishIntro();
+                finishGridActivity();
             }
         }
     }
