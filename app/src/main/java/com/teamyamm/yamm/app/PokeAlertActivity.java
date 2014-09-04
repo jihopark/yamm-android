289ef93a0452faf3431196ed827c0f73bbc2df11
jihopark <lastspurt17@gmail.com>
2014-09-04 16:09:49 +0900

    Minor Design Fix to Poke Alert

--- app/src/main/java/com/teamyamm/yamm/app/PokeAlertActivity.java
+++ app/src/main/java/com/teamyamm/yamm/app/PokeAlertActivity.java
@@ -101,7 +101,7 @@ public class PokeAlertActivity extends Activity {
         positive.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                makeYammToast(content.getSender().getName() + "님한테 좋다고 했어요!", Toast.LENGTH_SHORT);
+                makeYammToast(findFriendName(content.getSender(), getFriendList()) + "님한테 좋다고 했어요!", Toast.LENGTH_SHORT);
                 YammAPIAdapter.getTokenService().sendPokeResponse(new YammAPIService.RawPokeMessage(uids, true, content.getDish().getId()), callback);
                 trackPokeResponseMixpanel(true);
                 finish();
@@ -110,7 +110,7 @@ public class PokeAlertActivity extends Activity {
         negative.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                makeYammToast(content.getSender().getName() + "님한테 딴거 먹자고 했어요!", Toast.LENGTH_SHORT);
+                makeYammToast(findFriendName(content.getSender(), getFriendList()) + "님한테 딴거 먹자고 했어요!", Toast.LENGTH_SHORT);
                 YammAPIAdapter.getTokenService().sendPokeResponse(new YammAPIService.RawPokeMessage(uids, false, content.getDish().getId()), callback);
                 trackPokeResponseMixpanel(false);
                 finish();
